9513d96afef45b2150535bb01a5c0ab3872e0be7
Iain Collins <me@iaincollins.com>
2024-10-03 13:53:11 +0100

    Improve query for market commodites

--- router/api/markets.js
+++ router/api/markets.js
@@ -6,8 +6,8 @@ module.exports = (router) => {
   // typically only avalible from a single specific market.
   router.get('/api/v1/market/:marketId/commodity/name/:commodityName', async (ctx, next) => {
     const { marketId, commodityName } = ctx.params
-    const commodity = await dbAsync.get('SELECT * FROM trade.commodities WHERE marketId = @marketId COLLATE NOCASE AND commodityName = @commodityName COLLATE NOCASE', { marketId, commodityName })
-    if (!commodity) return NotFoundResponse(ctx, 'Market / Commodity not found')
+    const commodity = await dbAsync.get('SELECT * FROM trade.commodities WHERE marketId = @marketId AND commodityName = @commodityName COLLATE NOCASE', { marketId, commodityName })
+    if (!commodity) return NotFoundResponse(ctx, 'Market and/or commodity not found')
     ctx.body = commodity
   })
 }
