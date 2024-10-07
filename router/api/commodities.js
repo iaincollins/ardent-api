b9b51f289a70f5945c36c6d98b562ea84f8b8ec4
Iain Collins <me@iaincollins.com>
2024-10-07 18:56:36 +0100

    Add experimental news ticker
    Will likely refactor this to be pre-generated and use cached response

--- router/api/commodities.js
+++ router/api/commodities.js
@@ -17,6 +17,51 @@ module.exports = (router) => {
     ctx.body = JSON.parse(fs.readFileSync(COMMODITIES_REPORT)).commodities
   })
 
+  // This is an undocumented and unsupproted route likely to change in future.
+  router.get('/api/v1-beta/commodities/ticker', async (ctx, next) => {
+    const stations = await dbAsync.all(`
+    SELECT * FROM stations.stations AS s WHERE
+      s.stationType != 'Fleet Carrier'
+      AND s.stationType IS NOT NULL
+    GROUP BY s.stationName
+    ORDER BY s.updatedAt DESC
+    LIMIT 50`)
+
+    const imports = []
+    for (const station of stations) {
+      const stationImport = await dbAsync.get(`
+      SELECT * FROM trade.commodities as c
+        WHERE c.marketId = @marketId
+        AND (c.demand > 1000 OR c.demand = 0)
+        AND c.demandBracket = 3
+        AND c.updatedAt > '${getISOTimestamp(`-1`)}'
+      ORDER BY c.sellPrice DESC
+      LIMIT 5`, { marketId: station.marketId })
+      if (stationImport) imports.push(stationImport)
+    }
+
+    const exports = []
+    for (const station of stations) {
+      const stationExport = await dbAsync.get(`
+      SELECT * FROM trade.commodities as c
+        WHERE c.marketId = @marketId
+        AND c.stock > 1000
+        AND c.stockBracket = 3
+        AND c.updatedAt > '${getISOTimestamp(`-1`)}'
+      ORDER BY c.buyPrice DESC
+      LIMIT 5`, { marketId: station.marketId })
+      if (stationExport) exports.push(stationExport)
+    }
+
+    const result = [
+        ...imports,
+        ...exports,
+      ].sort((a, b) => a.updatedAt.localeCompare(b.updatedAt)) // Sort results by recency
+      .filter((obj1, i, arr) => arr.findIndex(obj2 => (obj2.marketId === obj1.marketId)) === i) // Filter so only one entry for each station
+    
+    ctx.body = result
+  })
+
   router.get('/api/v1/commodities/core-systems-1000', async (ctx, next) => {
     ctx.body = JSON.parse(fs.readFileSync(CORE_SYSTEMS_1000_REPORT))
   })
