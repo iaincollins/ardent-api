67eb6a4a3edfb6d8840e51939ec99ce6be7197df
Iain Collins <me@iaincollins.com>
2025-01-26 02:31:45 +0000

    Refactor stats parser to handle new stats file format

--- index.js
+++ index.js
@@ -83,19 +83,13 @@ function printStats () {
         'Stations:\n' +
         `* Stations: ${stats.stations.stations.toLocaleString()}\n` +
         `* Fleet Carriers: ${stats.stations.carriers.toLocaleString()}\n` +
-        `* Station updates in last hour: ${stats.stations.updatedInLastHour.toLocaleString()}\n` +
         `* Station updates in last 24 hours: ${stats.stations.updatedInLast24Hours.toLocaleString()}\n` +
-        `* Station updates in last 7 days: ${stats.stations.updatedInLast7Days.toLocaleString()}\n` +
-        `* Station updates in last 30 days: ${stats.stations.updatedInLast30Days.toLocaleString()}\n` +
         'Trade:\n' +
         `* Station Markets: ${stats.trade.stations.toLocaleString()}\n` +
         `* Fleet Carrier Markets: ${stats.trade.carriers.toLocaleString()}\n` +
         `* Trade systems: ${stats.trade.systems.toLocaleString()}\n` +
         `* Trade orders: ${stats.trade.tradeOrders.toLocaleString()}\n` +
-        `* Trade updates in last hour: ${stats.trade.updatedInLastHour.toLocaleString()}\n` +
         `* Trade updates in last 24 hours: ${stats.trade.updatedInLast24Hours.toLocaleString()}\n` +
-        `* Trade updates in last 7 days: ${stats.trade.updatedInLast7Days.toLocaleString()}\n` +
-        `* Trade updates in last 30 days: ${stats.trade.updatedInLast30Days.toLocaleString()}\n` +
         `* Unique commodities: ${stats.trade.uniqueCommodities.toLocaleString()}\n` +
         `Stats last updated: ${stats.timestamp}`
         : 'Stats not generated yet')
